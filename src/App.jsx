60f9f0c8b65066b96619c3bb394c8116675f480b
StanleyPauloIM <Stanleypauloim@gmail.com>
2025-08-30 16:11:39 +0200

    Adicionar navbar a todas paginas na App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 // Componente raiz que carrega as rotas da aplicação
 import React from 'react';
 import AppRoutes from './routes/AppRoutes';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <Navbar />
+      <AppRoutes />
+    </>
+  );
 }
 
 export default App;
