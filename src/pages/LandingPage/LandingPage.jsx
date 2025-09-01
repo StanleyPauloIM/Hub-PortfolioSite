973c7c993205ded09a3687c99fbce27dcfd547e4
StanleyPauloIM <Stanleypauloim@gmail.com>
2025-09-01 20:33:26 +0200

    Adicionar FeatureSection to LandingPage

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import styles from './LandingPage.module.css';
 import HeroSection from './components/HeroSection';
+import FeaturesSection from './components/FeaturesSection/FeaturesSection';
 
 const LandingPage = () => {
   return (
     <div className={styles.container}>
       <HeroSection />
+      <FeaturesSection />
     </div>
   );
 };
